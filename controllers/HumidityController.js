7691c2919415e5bfe50eb968b4897c1756864468
jinyoung1129 <jy11290@naver.com>
2020-11-19 00:10:32 +0900

    Add Function
    통계 페이지 습도 그래프 완료

--- controllers/HumidityController.js
+++ controllers/HumidityController.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const Humidity = require('./../models/Humidity');
 
+const date = new Date();
+const thisMonth = ''+ (date.getMonth()+1)
+const nextMonth = ''+ (date.getMonth()+2)
+const lastMonth = ''+ (date.getMonth())
+const thisYear = date.getFullYear()
+
+if (thisMonth.length < 2) thisMonth = '0' + thisMonth;
+if (nextMonth.length < 2) nextMonth = '0' + nextMonth;
+if (lastMonth.length < 2) lastMonth = '0' + lastMonth;
+
+const tMstart = new Date([thisYear, thisMonth].join('-'));
+const tMend = new Date([thisYear, nextMonth].join('-'));
+
+const lMstart = new Date([thisYear, lastMonth].join('-'));
+const lMend = new Date([thisYear, thisMonth].join('-'));
+
 router.use(bodyParser.urlencoded({ extended:true}));
 
 router.post('/', function(req, res) {
@@ -21,6 +37,106 @@ router.get('/', function(req, res) {
         if(err) return res.status(500).send("Humidity Select Fail");
         res.status(200).send(humidity);
     }).sort({ "_id": -1 }).limit(10);
+});
+
+// 이번 달 습도
+router.get('/this-month', function(req, res){
+    Humidity.find({date: {$gte: tMstart, $lt: tMend}}, function(err, data){
+        let subData = new Array();
+        let humidityData = new Array();
+
+        data.forEach((item) => {
+            subData.push({
+                date: getDate(item.date),
+                humidity: item.humidity
+            })
+        })
+        
+        first = subData[0];
+        let sumData = new Array();
+        subData.forEach((item, index) => {
+            let avg = 0;
+            if(index == (subData.length-1)){
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                humidityData.push({
+                    "date": first.date,
+                    "humidity": avg.toFixed(1)
+                })
+            }
+
+            if(first.date == item.date) {
+                sumData.push(item.humidity)
+            } else {
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                humidityData.push({
+                    "date": first.date,
+                    "humidity": avg.toFixed(1)
+                })
+                first = subData[index]
+                
+            }
+        })
+
+        res.send(humidityData)
+    }).sort({ "date": 1 })
+});
+
+// 지난 달 습도
+router.get('/last-month', function(req, res){
+    Humidity.find({date: {$gte: lMstart, $lt: lMend}}, function(err, data){
+        let subData = new Array();
+        let humidityData = new Array();
+
+        data.forEach((item) => {
+            subData.push({
+                date: getDate(item.date),
+                humidity: item.humidity
+            })
+        })
+        
+        first = subData[0];
+        let sumData = new Array();
+        subData.forEach((item, index) => {
+            let avg = 0;
+            if(index == (subData.length-1)){
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                humidityData.push({
+                    "date": first.date,
+                    "humidity": avg.toFixed(1)
+                })
+            }
+
+            if(first.date == item.date) {
+                sumData.push(item.humidity)
+            } else {
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                humidityData.push({
+                    "date": first.date,
+                    "humidity": avg.toFixed(1)
+                })
+                first = subData[index]
+                
+            }
+        })
+
+        res.send(humidityData)
+    }).sort({ "date": 1 })
 });
 
 module.exports = router;
