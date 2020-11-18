0cf8b8234a0754c86f668a2e458f55c98421ce4e
jinyoung1129 <jy11290@naver.com>
2020-11-19 01:17:50 +0900

    Add Function
    통계 페이지 소음 그래프 완료

--- controllers/NoiseController.js
+++ controllers/NoiseController.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const Noise = require('./../models/Noise');
 
+const date = new Date();
+const thisMonth = ''+ (date.getMonth()+1)
+const nextMonth = ''+ (date.getMonth()+2)
+const lastMonth = ''+ (date.getMonth())
+const thisYear = date.getFullYear()
+
+if (thisMonth.length < 2) thisMonth = '0' + thisMonth;
+if (nextMonth.length < 2) nextMonth = '0' + nextMonth;
+if (lastMonth.length < 2) lastMonth = '0' + lastMonth;
+
+const tMstart = new Date([thisYear, thisMonth].join('-'));
+const tMend = new Date([thisYear, nextMonth].join('-'));
+
+const lMstart = new Date([thisYear, lastMonth].join('-'));
+const lMend = new Date([thisYear, thisMonth].join('-'));
+
 router.use(bodyParser.urlencoded({ extended:true}));
 
 router.post('/', function(req, res) {
@@ -21,6 +37,108 @@ router.get('/', function(req, res) {
         if(err) return res.status(500).send("Noise Select Fail");
         res.status(200).send(noise);
     }).sort({ "_id": -1 }).limit(10);
+});
+
+// 이번 달 소음
+router.get('/this-month', function(req, res){
+    Noise.find({date: {$gte: tMstart, $lt: tMend}}, function(err, data){
+        let subData = new Array();
+        let noiseData = new Array();
+
+        data.forEach((item) => {
+            subData.push({
+                date: getDate(item.date),
+                noise: item.noise
+            })
+        })
+
+        
+        first = subData[0];
+        let sumData = new Array();
+        subData.forEach((item, index) => {
+            let avg = 0;
+            if(index == (subData.length-1)){
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                noiseData.push({
+                    "date": first.date,
+                    "noise": avg.toFixed(1)
+                })
+            }
+
+            if(first.date == item.date) {
+                sumData.push(item.noise)
+            } else {
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                noiseData.push({
+                    "date": first.date,
+                    "noise": avg.toFixed(1)
+                })
+                first = subData[index]
+                
+            }
+        })
+
+        res.send(noiseData)
+    }).sort({ "date": 1 })
+});
+
+// 지난 달 소음
+router.get('/last-month', function(req, res){
+    Noise.find({date: {$gte: lMstart, $lt: lMend}}, function(err, data){
+        let subData = new Array();
+        let noiseData = new Array();
+
+        data.forEach((item) => {
+            subData.push({
+                date: getDate(item.date),
+                noise: item.noise
+            })
+        })
+
+        
+        first = subData[0];
+        let sumData = new Array();
+        subData.forEach((item, index) => {
+            let avg = 0;
+            if(index == (subData.length-1)){
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                noiseData.push({
+                    "date": first.date,
+                    "noise": avg.toFixed(1)
+                })
+            }
+
+            if(first.date == item.date) {
+                sumData.push(item.noise)
+            } else {
+                avg = 0;
+                for(let i=0; i<sumData.length; i++){
+                    avg += sumData[i];
+                }
+                avg = avg/sumData.length;
+                noiseData.push({
+                    "date": first.date,
+                    "noise": avg.toFixed(1)
+                })
+                first = subData[index]
+                
+            }
+        })
+
+        res.send(noiseData)
+    }).sort({ "date": 1 })
 });
 
 module.exports = router;
